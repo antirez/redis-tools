729aea3545a4b0f5bbf92b9d06b6d8965ca995d7
antirez <antirez@gmail.com>
2010-01-21 15:39:09 -0500

    added script to sync redis libs with tools libs

---
+++ update-all-libs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -f ~/hack/redis/adlist.[ch] .
+cp -f ~/hack/redis/ae.[ch] .
+cp -f ~/hack/redis/ae_*.c .
+cp -f ~/hack/redis/anet.[ch] .
+cp -f ~/hack/redis/config.h .
+cp -f ~/hack/redis/fmacros.h .
+cp -f ~/hack/redis/sds.[ch] .
+cp -f ~/hack/redis/zmalloc.[ch] .
